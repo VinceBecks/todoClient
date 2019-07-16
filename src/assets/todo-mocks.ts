81b10371a3563267e068e2c3f1498a284262be63
Vincent Becker <vincent.becker@openknowledge.de>
2019-07-16 11:47:19 +0200

    Create mock data

---
+++ src/assets/todo-mocks.ts
@@ -0,0 +1,22 @@
+import {Todo} from '../app/domain/Todo';
+
+export const TODOS: Todo [] = [
+  {
+    todoId: 1,
+    title: 'Clean up',
+    description: 'bath, living room',
+    state: 'open'
+  },
+  {
+    todoId: 2,
+    title: 'Buy in ',
+    description: 'milk, bread, honey',
+    state: 'open'
+  },
+  {
+    todoId: 1,
+    title: 'Beat someone',
+    description: 'Like lukas',
+    state: 'open'
+  },
+];
